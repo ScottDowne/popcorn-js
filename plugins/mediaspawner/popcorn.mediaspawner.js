c39c452d107919464aff8c7391e058b007a74f5a
Matthew Schranz <schranz.m@gmail.com>
2012-02-13 10:52:59 -0500

    Initial media spawner plugin work

---
+++ plugins/mediaspawner/popcorn.mediaspawner.js
@@ -0,0 +1,83 @@
+( function( Popcorn, global ) {
+
+  Popcorn.plugin( "mediaspawner" , {
+    manifest: {
+      about: {
+        name: "Popcorn Media Spawner Plugin",
+        version: "0.1",
+        author: "Matthew Schranz, @mjschranz",
+        website: "mschranz.wordpress.com"
+      },
+      options: {
+        mediaType: {
+          elem: "select",
+          options: [ "YOUTUBE", "VIMEO", "SOUNDCLOUD"],
+          label: "Media Type:"
+        },
+        caption: {
+          elem: "input",
+          type: "text",
+          label: "Media Caption:"
+        },
+        mediaSource: {
+          elem: "input",
+          type: "text",
+          label: "Media Source:"
+        },
+        target: "tumblr-container",
+        start: {
+          elem: "input",
+          type: "number",
+          label: "Start_Time"
+        },
+        end: {
+          elem: "input",
+          type: "number",
+          label: "End_Time"
+        }
+      }
+    },
+    _setup: function( options ) {
+      var target = document.getElementById( options.target ),
+          validType,
+          videoId,
+          pop;
+
+      // Valid types of retrieval requests
+      validType = function( type ) {
+        return ( [ "youtube", "vimeo", "soundcloud" ].indexOf( type ) > -1 );
+      };
+
+      // Lowercase the types incase user enters it in another way
+      options.mediaType = options.mediaType.toLowerCase();
+
+      !validType( options.mediaType ) && Popcorn.error( "Invalid Media Type.");
+
+      ( !options.mediaSource || !options.target) && Popcorn.error( "Must include a Media Source and target container.");
+
+      // Create seperate container for plugin
+      options._container = document.createElement( "div" );
+      videoId = "mediaSpawnerdiv-" + Popcorn.guid();
+      options._container.id = videoId;
+      options._container.innerHTML = options.caption + "<br/>";
+      
+      pop = Popcorn.youtube( "#" + videoId, options.mediaSource );
+
+      options._container.style.display = "none";
+      target && target.appendChild( options._container );
+    },
+    start: function( event, options ){
+      if ( options._container ) {
+        options._container.style.display = "";
+      }
+    },
+    end: function( event, options ){
+      if( options._container ) {
+        options._container.style.display = "none";
+      }
+    },
+    _teardown: function( options ){
+      document.getElementById( options.target ) && document.getElementById( options.target ).removeChild( options._container );
+    }
+  });
+})( Popcorn, this );
